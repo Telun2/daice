f41c08fc17cad24e765392f1af23bc444c2fc294
Telun2 <ayukanyanman@gmail.com>
2025-07-01 17:59:17 +0900

    Dice game: initial revision

---
+++ dice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand((unsigned)time(NULL));
+
+    int n=0, m=0;
+
+    n = (rand()%6)+1;
+    m = (rand()%6)+1;
+
+    printf("Rolling dice...\n");
+    printf("Die 1:%d\n",n);
+    printf("Die 2:%d\n",m);
+    printf("Total value:%d\n",n+m);
+
+    return 0;
+}
